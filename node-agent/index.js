8b36f6f29653a0c15a66a27075d37b0627f7015f
mghaniii <ghanioy12346@gmail.com>
2025-12-03 20:40:37 +0700

    index js pada folder server sedikit

--- node-agent/index.js
+++ node-agent/index.js
@@ -1,11 +1,14 @@
+// 1. Mengimpor library socket.io-client
 const io = require("socket.io-client");
+
+// 2. Menghubungkan agent ke server Socket.IO di http://localhost:3000
 const socket = io("http://localhost:3000");
 
-
-
+// 3. Fungsi untuk membuat data "trafik jaringan" palsu (simulasi)
 function buatDataTrafik() {
   const packetRate = Math.floor(Math.random() * 200); 
-  
+  // nilai acak antara 0–199
+
   const timestamp = Date.now(); 
   // waktu sekarang dalam bentuk milidetik sejak 1970
 
@@ -21,11 +24,3 @@ setInterval(() => {
   socket.emit("log", data); // kirim event bernama "log" ke server
   console.log("Mengirim log:", data);
 }, 1000);
-
-setInterval (()=> {
-    const data = buatDataTrafik();
-     socket.emit("log", data); // kirim event bernama "log" ke server
-  console.log("Mengirim log:", data);
-
-
-}, 1000)
--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const cors = require("cors");
 const ss = require("simple-statistics");
 
 const app = express();
+app.use(cors()); // mengizinkan akses dari origin lain (dashboard)
+
 const server = http.createServer(app);
-const io = socketIO(server);
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let logs = [];
+
+
+io.on("connection", (socket) => {
+  console.log("Client terhubung:", socket.id);
